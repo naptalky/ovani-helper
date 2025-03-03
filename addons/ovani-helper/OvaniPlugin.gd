768b4b25a064ae7e0a17c80513179a9d51ba410b
Filip Motyczka <mefjak@gmail.com>
2025-03-03 18:37:26 +0100

    moved options to 'Create New'

--- addons/ovani-helper/OvaniPlugin.gd
+++ addons/ovani-helper/OvaniPlugin.gd
@@ -5,7 +5,7 @@ var plugin:EditorContextMenuPlugin
 
 func _enter_tree() -> void:
 	var plugin = Plugin.new()
-	add_context_menu_plugin(EditorContextMenuPlugin.CONTEXT_SLOT_FILESYSTEM, plugin)
+	add_context_menu_plugin(EditorContextMenuPlugin.CONTEXT_SLOT_FILESYSTEM_CREATE, plugin)
 
 func _exit_tree() -> void:
 	remove_context_menu_plugin(plugin)
